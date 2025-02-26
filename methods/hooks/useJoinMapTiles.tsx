bd2115e431c59dd02271c6a73afe2f1f3e8fa19b
Ciabatos <133810024+Ciabatos@users.noreply.github.com>
2025-02-26 10:46:06 +0100

    Create useJoinMapTiles.tsx

---
+++ methods/hooks/useJoinMapTiles.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { useEffect, useState } from "react"
+import { joinMapTilesClient } from "@/methods/functions/joinMapTilesClient"
+import { mapTilesAtom } from "@/store/atoms"
+import type { TjoinedMapTile } from "@/methods/functions/joinMapTilesClient"
+import type { TMapTiles } from "@/db/postgresMainDatabase/schemas/map/tables/mapTiles"
+import type { TMapTerrainTypes } from "@/db/postgresMainDatabase/schemas/map/tables/mapTerrainTypes"
+
+export function useUpdatedTiles(
+  joinedMapTiles: Record<string, TjoinedMapTile>,
+  terrainTypesById: Record<number, TMapTerrainTypes>
+) {
+  const [updatedTiles, setUpdatedTiles] = useState(joinedMapTiles)
+  const mapTiles = useAtomValue(mapTilesAtom)
+  
+  useEffect(() => {
+    if (mapTiles) {
+      const updatedTiles = joinMapTilesClient(joinedMapTiles, mapTiles, terrainTypesById)
+      setUpdatedTiles(updatedTiles)
+    }
+  }, [joinedMapTiles, mapTiles, terrainTypesById])
+
+  return updatedTiles
+}
