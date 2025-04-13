071f5b9218deff863d0867a7285bae50f0df95e5
Ciabatos <pszabat001@gmail.com>
2025-04-13 20:13:19 +0200

    commit

--- app/api/inventory-slots/[playerId]/route.tsx
+++ app/api/inventory-slots/[playerId]/route.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { auth } from "@/auth"
 import { getInventorySlots } from "@/db/postgresMainDatabase/schemas/players/tables/inventories"
 import { NextRequest, NextResponse } from "next/server"
 
@@ -8,7 +9,12 @@ type TypeParams = {
 
 export async function GET(request: NextRequest, { params }: { params: TypeParams }): Promise<NextResponse> {
   const playerId = (await params).playerId
+  const session = await auth()
+  const dad = session?.user?.playerId
 
+  if (!dad || isNaN(dad)) {
+    return NextResponse.json({ success: false })
+  }
   // const searchQueryParams = request.nextUrl.searchParams
   // const login = searchQueryParams.get("login")
 
