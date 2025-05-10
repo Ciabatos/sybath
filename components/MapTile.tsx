cc569d1d6e1794d037bdf10f376745c6bdc43be8
Ciabatos <pszabat001@gmail.com>
2025-05-10 12:17:27 +0200

    remaster hooks

--- components/MapTile.tsx
+++ components/MapTile.tsx
@@ -2,11 +2,8 @@
 
 import MapTileLayerHandling from "@/components/MapTileLayerHandling"
 import style from "@/components/styles/MapTile.module.css"
-import { combineImages } from "@/methods/functions/combineImages"
 import { TJoinedMapTile } from "@/methods/functions/joinMapTiles"
-import { useCreateBackgroundImage } from "@/methods/hooks/useCreateBackgroundImage"
-import { useCreateLandscapeImage } from "@/methods/hooks/useCreateLandscapeImage"
-import { useCreatePlayerImage } from "@/methods/hooks/useCreatePlayerImage"
+import { useCreateImage } from "@/methods/hooks/useCreateImage"
 import { useMapTileClickHandling } from "@/methods/hooks/useMapTileClickHandling"
 
 interface Props {
@@ -14,36 +11,37 @@ interface Props {
 }
 
 export default function MapTile({ tile }: Props) {
-  const backgroundImage = useCreateBackgroundImage(tile.terrainTypes.image_url)
-  const landscapeImage = useCreateLandscapeImage(tile.landscapeTypes?.image_url)
-  const playerImage = useCreatePlayerImage(tile.playerVisibleMapData?.player_image_url)
+  const { createPlayerImage, createLandscapeImage, createBackgroundImage, combineImages } = useCreateImage()
+
+  const backgroundImage = createBackgroundImage(tile.terrainTypes.image_url)
+  const landscapeImage = createLandscapeImage(tile.landscapeTypes?.image_url)
+  const playerImage = createPlayerImage(tile.playerVisibleMapData?.player_image_url)
   const combinedImages = combineImages(landscapeImage, backgroundImage)
+
   const { handleClickOnMapTile } = useMapTileClickHandling()
 
-  const handleClick = (x: number, y: number) => {
+  const handleButtonClick = (x: number, y: number) => {
     handleClickOnMapTile(x, y)
   }
 
   return (
-    <>
+    <div
+      className={style.BackgroundImage}
+      onDoubleClick={() => handleButtonClick(tile.mapTile.x, tile.mapTile.y)}
+      style={{
+        gridColumnStart: tile.mapTile.x,
+        gridRowStart: tile.mapTile.y,
+        backgroundImage: combinedImages,
+      }}>
       <div
-        className={style.BackgroundImage}
-        onDoubleClick={() => handleClick(tile.mapTile.x, tile.mapTile.y)}
+        className={style.PlayerImage}
         style={{
-          gridColumnStart: tile.mapTile.x,
-          gridRowStart: tile.mapTile.y,
-          backgroundImage: combinedImages,
-        }}>
-        <div
-          className={style.PlayerImage}
-          style={{
-            backgroundImage: playerImage,
-          }}></div>
-        <div>
-          {tile.mapTile.x}, {tile.mapTile.y}
-        </div>
-        <MapTileLayerHandling tile={tile} />
+          backgroundImage: playerImage,
+        }}></div>
+      <div>
+        {tile.mapTile.x}, {tile.mapTile.y}
       </div>
-    </>
+      <MapTileLayerHandling tile={tile} />
+    </div>
   )
 }
---
+++ methods/hooks/useCreateImage.tsx
@@ -0,0 +1,42 @@
+"use client"
+import Image from "next/image"
+
+export function useCreateImage() {
+  function createImageFromUrl(imageUrl: string | undefined, type: string) {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+    if (!imageUrl) {
+      return ""
+    }
+    const imageSrc = `/${type}/${imageUrl}`
+    const imageComponent = (
+      <Image
+        src={imageSrc}
+        layout="fill"
+        objectFit="contain"
+        quality={100}
+        alt={imageUrl || ""}
+        priority
+      />
+    )
+    const optimizedImageUrl = `url(${baseUrl}${imageComponent.props.src})`
+    return optimizedImageUrl
+  }
+
+  function createPlayerImage(imageUrl: string | undefined) {
+    return createImageFromUrl(imageUrl, "playersPicture")
+  }
+
+  function createLandscapeImage(imageUrl: string | undefined) {
+    return createImageFromUrl(imageUrl, "landscapeTypePicture")
+  }
+
+  function createBackgroundImage(imageUrl: string | undefined) {
+    return createImageFromUrl(imageUrl, "terrainTypePicture")
+  }
+
+  const combineImages = (...images: string[]): string => {
+    return images.filter(Boolean).join(", ")
+  }
+
+  return { createPlayerImage, createLandscapeImage, createBackgroundImage, combineImages }
+}
