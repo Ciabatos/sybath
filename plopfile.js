4fbaae0e106b2ba6ea64ed431cb164ffd535a4b5
Ciabatos <133810024+Ciabatos@users.noreply.github.com>
2025-10-06 12:52:34 +0200

    Update plopfile.js

--- plopfile.js
+++ plopfile.js
@@ -1,9 +1,10 @@
 const getTable = require("./plop-generators/getTable")
+const getProcedure = require("./plop-generators/getProcedure")
 
 module.exports = function (plop) {
   plop.setHelper("eq", (a, b) => a === b)
 
 
   getTable(plop)
-
+  getProcedure(plop)
 }
