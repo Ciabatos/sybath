649403900473158e7498dee35db17111af2b0dc0
Ciabatos <pszabat001@gmail.com>
2025-12-18 21:20:06 +0100

    commit

--- db/backup/fc1.sql
+++
@@ -1,2 +0,0 @@
-ALTER FUNCTION world.player_movement (int4, jsonb)
-RENAME TO do_player_movement;
--- db/backup/fc2.sql
+++
@@ -1,3 +0,0 @@
-DROP FUNCTION inventory.add_item_to_inventory (int4, int4, int4);
-
-create schema utils
