edffb1afc32273ea72b7dbf0d3142b0de6675d5a
Piotr Szabat <piotr.szabat@rawlplug.com>
2025-12-18 16:01:05 +0100

    commit

--- db/backup/fc3.sql
+++ db/backup/fc3.sql
@@ -1,11 +1,16 @@
-CREATE OR REPLACE FUNCTION utils.raise_error(p_message text)
+CREATE OR REPLACE FUNCTION utils.raise_error(p_message text, VARIADIC p_args text[])
 RETURNS void
 LANGUAGE plpgsql
 AS $$
+DECLARE
+    msg text;
 BEGIN
+    -- jeśli nie podano argumentów, używa samego tekstu
+    -- przykład użycia PERFORM utils.raise_error('Cannot add % units of item %', p_quantity::text, p_item_id::text);
+    msg := format(p_message, VARIADIC p_args);
+
     RAISE EXCEPTION
-        USING
-            ERRCODE = 'P0001',
-            MESSAGE = p_message;
+        USING ERRCODE = 'P0001',
+              MESSAGE = msg;
 END;
 $$;
