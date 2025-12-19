ad22d337478bbc3152901c20f921eacf241f810b
Piotr Szabat <piotr.szabat@rawlplug.com>
2025-12-19 11:22:22 +0100

    commit

--- providers/swr-provider.tsx
+++ providers/swr-provider.tsx
@@ -3,20 +3,22 @@ import { SWRConfig } from "swr"
 
 const storedEtagMap = new Map<string, string>()
 
-const fetchWithETag = async (url: string) => {
+async function fetchWithETag<T>(url: string): Promise<T> {
   const headers: Record<string, string> = {}
   const storedEtag = storedEtagMap.get(url)
 
   if (storedEtag) headers["If-None-Match"] = storedEtag
 
   const res = await fetch(url, { headers })
-  if (res.status === 304) {
-    // Zwróć poprzednie dane z cache SWR (SWR to obsłuży)
-    return undefined
+
+  if (!res.ok && res.status !== 304) {
+    throw new Error(`Fetch error: ${res.status}`)
   }
+
   const newEtag = res.headers.get("etag")
   if (newEtag) storedEtagMap.set(url, newEtag)
-  return res.json()
+
+  return res.json() as Promise<T>
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
