a865a9446c61ced6a34ce9bd975d0111802a57c4
Ciabatos <pszabat001@gmail.com>
2026-01-20 21:53:09 +0100

    commit

--- methods/hooks/players/composite/useActivePlayerSwitchProfiles.ts
+++ methods/hooks/players/composite/useActivePlayerSwitchProfiles.ts
@@ -1,15 +1,23 @@
 "use client"
 
+import { doSwitchActivePlayerAction } from "@/methods/actions/players/doSwitchActivePlayerAction"
 import { usePlayerId } from "@/methods/hooks/players/composite/usePlayerId"
 import { useFetchActivePlayerSwitchProfiles } from "@/methods/hooks/players/core/useFetchActivePlayerSwitchProfiles"
+import { useMutateActivePlayer } from "@/methods/hooks/players/core/useMutateActivePlayer"
 import { activePlayerSwitchProfilesAtom } from "@/store/atoms"
 import { useAtomValue } from "jotai"
 
 export function useActivePlayerSwitchProfiles() {
   const { playerId } = usePlayerId()
+  const { mutateActivePlayer } = useMutateActivePlayer()
 
   useFetchActivePlayerSwitchProfiles({ playerId })
   const activePlayerSwitchProfiles = useAtomValue(activePlayerSwitchProfilesAtom)
 
-  return { activePlayerSwitchProfiles }
+  function switchPlayer(newPlayerId: number) {
+    doSwitchActivePlayerAction({ playerId: playerId, switchToPlayerId: newPlayerId })
+    mutateActivePlayer({ id: newPlayerId })
+  }
+
+  return { activePlayerSwitchProfiles, switchPlayer }
 }
--- methods/hooks/players/composite/usePlayerId.ts
+++ methods/hooks/players/composite/usePlayerId.ts
@@ -1,23 +1,14 @@
 "use client"
-import { doSwitchActivePlayerAction } from "@/methods/actions/players/doSwitchActivePlayerAction"
 import { useFetchActivePlayer } from "@/methods/hooks/players/core/useFetchActivePlayer"
-import { useMutateActivePlayer } from "@/methods/hooks/players/core/useMutateActivePlayer"
 import { activePlayerAtom } from "@/store/atoms"
 import { useAtomValue } from "jotai"
 
 export function usePlayerId() {
-  const { mutateActivePlayer } = useMutateActivePlayer()
-
   useFetchActivePlayer()
   const activePlayerData = useAtomValue(activePlayerAtom)
 
   const activePlayer = Object.values(activePlayerData)[0] ?? null
   const playerId = activePlayer?.id
 
-  function switchPlayer(newPlayerId: number) {
-    doSwitchActivePlayerAction({ playerId: playerId, switchToPlayerId: newPlayerId })
-    mutateActivePlayer({ id: newPlayerId })
-  }
-
-  return { playerId, switchPlayer }
+  return { playerId }
 }
