0f1a3ea1db2e0015c3d9abddad22eb960fc3b2a4
Ciabatos <pszabat001@gmail.com>
2026-04-03 00:08:36 +0200

    commit

--- components/attributes/AllSkills.tsx
+++ components/attributes/AllSkills.tsx
@@ -1,16 +1,28 @@
 "use client"
 import Skill from "@/components/attributes/Skill"
+import { Button } from "@/components/ui/button"
 import getIcon from "@/methods/functions/icons/getIcon"
 import { useAllSKills } from "@/methods/hooks/attributes/composite/useAllSkills"
+import { useModalRightCenter } from "@/methods/hooks/modals/useModalRightCenter"
+import { X } from "lucide-react"
 import styles from "./styles/AllSkills.module.css"
 
 export default function AllSkills() {
   const { skills } = useAllSKills()
+  const { resetModalRightCenter } = useModalRightCenter()
 
   return (
     <>
       <div className={styles.skillsContainer}>
         <div className={styles.skillsGrid}>
+          <Button
+            onClick={() => resetModalRightCenter()}
+            variant='ghost'
+            size='icon'
+            className={styles.closeButton}
+          >
+            <X className={styles.closeButtonIcon} />
+          </Button>
           {Object.values(skills).map((skill) => (
             <Skill
               key={skill.id}
