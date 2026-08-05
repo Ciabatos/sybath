9b9dd89b0554cf567b08199bfa57bab0f76c6054
Piotr Szabat <piotr.szabat@rawlplug.com>
2026-08-05 10:26:22 +0200

    reactLazy to nextDynamic test

---
+++ types/panels/panelLeftTopBar.tsx
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT EDIT MANUALLY
+import { EPanelsLeftTopBar } from "@/types/enumeration/EPanelsLeftTopBar"
+import dynamic from "next/dynamic"
+import React from "react"
+
+export const panelLeftTopBar: Record<EPanelsLeftTopBar, React.ComponentType<any> | null> = {
+  [EPanelsLeftTopBar.Inactive]: null,
+  [EPanelsLeftTopBar.PlayerPanel]: dynamic(() => import("@/components/players/PlayerPanel"), {
+    loading: () => <p>Ładowanie panelu gracza...</p>,
+  }),
+  [EPanelsLeftTopBar.PlayerRibbonTop]: dynamic(() => import("@/components/players/PlayerRibbonTop"), {
+    loading: () => <p>Ładowanie...</p>,
+  }),
+  [EPanelsLeftTopBar.PlayerSquad]: dynamic(() => import("@/components/squad/PlayerSquad"), {
+    loading: () => <p>Ładowanie składu...</p>,
+  }),
+}
+
