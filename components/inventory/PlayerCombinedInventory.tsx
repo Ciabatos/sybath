ac8f5da8318c87a6b7733899dacd6a1f1f04a15a
Piotr Szabat <piotr.szabat@rawlplug.com>
2026-08-06 12:59:11 +0200

    commit

--- components/inventory/PlayerCombinedInventory.tsx
+++ components/inventory/PlayerCombinedInventory.tsx
@@ -2,12 +2,21 @@
 
 import { PlayerContainer } from "@/components/inventory/PlayerContainer"
 import { PlayerGear } from "@/components/inventory/PlayerGear"
+import { Button } from "@/components/ui/button"
+import { useModalTopCenter } from "@/methods/hooks/modals/useModalTopCenter"
+import { EPanelsTopCenter } from "@/types/enumeration/EPanelsTopCenter"
 import styles from "./styles/PlayerCombinedInventory.module.css"
 
 export function PlayerCombinedInventory() {
+  const { openModalTopCenter } = useModalTopCenter()
+
+  function openTrades() {
+    openModalTopCenter(EPanelsTopCenter.TradeList)
+  }
+
   return (
     <div className={styles.wrapper}>
-      <div>Trade requests</div>
+      <Button onClick={openTrades}>Trade requests</Button>
       <PlayerGear />
       <PlayerContainer />
     </div>
