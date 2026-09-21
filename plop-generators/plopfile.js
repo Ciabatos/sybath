d048ec695f5b2da207721325dbb765e63c1616f7
Piotr Szabat <pszabat001@gmail.com>
2026-09-22 00:44:25 +0200

    formatted

--- plop-generators/plopfile.js
+++ plop-generators/plopfile.js
@@ -6,18 +6,6 @@ import replayHistory from "./generators/replayHistory/index.js"
 import rollback from "./generators/rollback/index.js"
 
 async function configurePlop(plop) {
-  const pathsToFormat = [
-    "store/atoms.ts",
-    "db/postgresMainDatabase/schemas",
-    "app/api",
-    "methods/hooks",
-    "methods/server-fetchers",
-    "methods/services",
-    "methods/actions",
-  ]
-
-  plop.setHelper("eq", (a, b) => a === b)
-
   plop.setHelper("json", (context) => JSON.stringify(context, null, 2))
 
   // createPanels(plop)
